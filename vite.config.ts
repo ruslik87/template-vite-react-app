2d7608a09a071eb5204e350ad6c6bbf545ac1cb2
Ruslan Miroshnychenko <rusikm87@gmail.com>
2023-03-04 14:12:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const cwd = process.cwd();
+const REPOSITORY_NAME = cwd.toString().split('\\').pop();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/name_repo/'
+  base: `/${REPOSITORY_NAME}/`
 })
