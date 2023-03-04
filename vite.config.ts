763ea2e075fcf544bea04e6b2c85d7cc446af0e7
Ruslan Miroshnychenko <rusikm87@gmail.com>
2023-03-04 14:31:12 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const cwd = process.cwd();
-const REPOSITORY_NAME = cwd.toString().split('\\').pop();
+const REPOSITORY_NAME = cwd.split('\\').pop();
 
 // https://vitejs.dev/config/
 export default defineConfig({
